d8309616e0ff5458bc4f5b50d771123e40305d16
Vince Force <vincentforce@yahoo.fr>
2018-10-04 14:51:42 +0200

    Removed commented code

--- backend/level3/main.rb
+++ backend/level3/main.rb
@@ -1,24 +1,5 @@
 require '../common/level_functions'
 
-# def car_rental_price(cars, rental, options, options_day_prices)
-# 	rental_id = rental["id"]
-# 	car_id = rental["car_id"]
-# 	duration = (Date.parse(rental["end_date"]) - Date.parse(rental["start_date"])).to_i + 1
-# 	distance = rental["distance"].to_i
-
-# 	car = cars.find {|c| c["id"] == car_id}
-# 	price_per_day = car["price_per_day"].to_i
-# 	price_per_km = car["price_per_km"].to_i
-
-# 	price_days = price_days_compute(duration, price_per_day)
-# 	price_distance = distance * price_per_km
-# 	price = price_days + price_distance
-
-# 	commission = commission_compute(price, duration)
-
-# 	return { "id" => rental_id, "price" => price , "commission" => commission}
-# end
-
 level = 3
 crp_class = CarRentalPriceLevel3
 
